00ea751f6a46d6f019cdec35f6e348b454cd7601
Cristian Corrales <crisdevs95@gmail.com>
2023-11-01 22:26:08 -0400

    Format the text of string notes to be more clear as to what string it represents

--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -2,17 +2,19 @@ import React from "react";
 
 export default function Card(props){
    const {name, stringNumber, _id} = props.tuningProfile;
+   console.log();
    const tunings = Object.entries(props.tuningProfile.tunings);
    const deleteTuning = () =>{
     fetch(`api/tunings/${_id}`, {method:'DELETE'});
    }
+   console.log(tunings);
    
     return(
         <div className = "text-white border w-52 flex flex-col items-center rounded-lg py-2">
             <h2 >{name}</h2>
             <p>Number of Strings: {stringNumber}</p>
             <ul>
-               {tunings.map((tuning) =><li key = {tuning[0]}>{tuning[1]}</li>)}
+               {tunings.map((tuning) =><li key = {tuning[0]}>{tuning[0]}: {tuning[1]}</li>)}
             </ul>
             <div className = "mt-5">
                 <button className = "mr-3 border rounded p-2">Edit</button>
